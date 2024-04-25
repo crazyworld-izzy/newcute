5be533cd374b1b30cc1e8e653a61869c09455088
Akash <126413936+crazyworld-izzy@users.noreply.github.com>
2024-04-25 23:06:48 +0530

    Update welcome.py

--- VIPMUSIC/plugins/tools/welcome.py
+++ VIPMUSIC/plugins/tools/welcome.py
@@ -117,8 +117,8 @@ def welcomepic(pic, user, chatname, id, uname, brightness_factor=1.3):
     draw.text((1530, 1230), f': {id}', fill=None, font=font, stroke_fill=(255, 255, 255), stroke_width=0)
     
     # Draw user's username with white fill and green border
-    draw.text((1030, 750), f': {uname}', fill=(255, 255, 255), font=font)
-    draw.text((1030, 750), f': {uname}', fill=None, font=font, stroke_fill=(0, 128, 0), stroke_width=6)
+    draw.text((2000, 750), f': {uname}', fill=(255, 255, 255), font=font)
+    draw.text((2000, 750), f': {uname}', fill=None, font=font, stroke_fill=(0, 128, 0), stroke_width=6)
     
     # Resize photo and position
     pfp_position = (255, 323)
