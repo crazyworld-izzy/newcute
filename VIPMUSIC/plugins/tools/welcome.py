02133c7935a0416e77af24d0d2468123ac671a41
Akash <126413936+crazyworld-izzy@users.noreply.github.com>
2024-04-28 13:52:19 +0530

    Update welcome.py

--- VIPMUSIC/plugins/tools/welcome.py
+++ VIPMUSIC/plugins/tools/welcome.py
@@ -114,7 +114,7 @@ def welcomepic(pic, user, chatname, id, uname, brightness_factor=1.3):
     
     # Draw user's id with shining blue fill and white border
     draw.text((1600, 1300), f': {id}', fill=(0, 0, 139))
-    draw.text((1600, 1300), f': {id}', fill=None, font=font, stroke_fill=(255, 255, 255), stroke_width=0)
+    draw.text((1600, 1300), f': {id}', fill=None, font=font, stroke_fill=(247, 0, 255), stroke_width=6)
     
     # Draw user's username with white fill and green border
     draw.text((2000, 1520), f': {uname}', fill=(255, 255, 255), font=font)
