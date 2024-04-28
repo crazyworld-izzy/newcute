362e7c6eaae9dad1659914cd57d6d8ac4a213c62
Akash <126413936+crazyworld-izzy@users.noreply.github.com>
2024-04-28 15:28:01 +0530

    Update welcome.py

--- VIPMUSIC/plugins/tools/welcome.py
+++ VIPMUSIC/plugins/tools/welcome.py
@@ -109,19 +109,19 @@ def welcomepic(pic, user, chatname, id, uname, brightness_factor=1.3):
     welcome_font = ImageFont.truetype('VIPMUSIC/assets/font.ttf', size=45)
     
     # Draw user's name with shining red fill and dark saffron border
-    draw.text((1800, 1090), f': {user}', fill=(255, 0, 0), font=font)
-    draw.text((1800, 1090), f': {user}', fill=None, font=font, stroke_fill=(255, 153, 51), stroke_width=6)
+    draw.text((1820, 1090), f': {user}', fill=(255, 0, 0), font=font)
+    draw.text((1820, 1090), f': {user}', fill=None, font=font, stroke_fill=(255, 153, 51), stroke_width=6)
     
     # Draw user's id with shining blue fill and white border
-    draw.text((1600, 1290), f': {id}', fill=(0, 0, 139))
-    draw.text((1600, 1290), f': {id}', fill=None, font=font, stroke_fill=(247, 0, 255), stroke_width=6)
+    draw.text((1620, 1290), f': {id}', fill=(0, 0, 139))
+    draw.text((1620, 1290), f': {id}', fill=None, font=font, stroke_fill=(247, 0, 255), stroke_width=6)
     
     # Draw user's username with white fill and green border
     draw.text((2000, 1520), f': {uname}', fill=(255, 255, 255), font=font)
     draw.text((2000, 1520), f': {uname}', fill=None, font=font, stroke_fill=(0, 128, 0), stroke_width=6)
     
     # Resize photo and position
-    pfp_position = (255, 360)
+    pfp_position = (255, 355)
     background.paste(pfp, pfp_position, pfp)
 
     # Calculate circular outline coordinates
