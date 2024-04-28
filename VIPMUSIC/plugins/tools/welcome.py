7b4551b6ff426641dc90c67f9cfe7be5950a558b
Akash <126413936+crazyworld-izzy@users.noreply.github.com>
2024-04-29 02:25:13 +0530

    Update welcome.py

--- VIPMUSIC/plugins/tools/welcome.py
+++ VIPMUSIC/plugins/tools/welcome.py
@@ -209,8 +209,8 @@ async def greet_new_member(_, member: ChatMemberUpdated):
             welcomeimg = welcomepic(
                 pic, user.first_name, member.chat.title, user.id, user.username
             )
-            button_text = "๏ ᴠɪᴇᴡ ɴᴇᴡ ᴍᴇᴍʙᴇʀ ๏"
-            add_button_text = "๏ ᴋɪᴅɴᴀᴘ ᴍᴇ ๏"
+            button_text = "🍷 𝐍𖽞𖽮 𝐌𖽞𖽧𖽜𖽞𖽷 😻"
+            add_button_text = "🍷 𝐊𖽹𖾓𖽡𖽖𖽳 𝐌𖽞 😻"
             deep_link = f"tg://openmessage?user_id={user.id}"
             add_link = f"https://t.me/{app.username}?startgroup=true"
             temp.MELCOW[f"welcome-{member.chat.id}"] = await app.send_photo(
