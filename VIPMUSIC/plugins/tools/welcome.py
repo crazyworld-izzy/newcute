4010b2e80d536c5f1ebb6b7095ad0a91e8f5e37e
Akash <126413936+crazyworld-izzy@users.noreply.github.com>
2024-04-28 11:51:48 +0530

    Update welcome.py

--- VIPMUSIC/plugins/tools/welcome.py
+++ VIPMUSIC/plugins/tools/welcome.py
@@ -113,12 +113,12 @@ def welcomepic(pic, user, chatname, id, uname, brightness_factor=1.3):
     draw.text((1000, 700), f': {user}', fill=None, font=font, stroke_fill=(255, 153, 51), stroke_width=6)
     
     # Draw user's id with shining blue fill and white border
-    draw.text((1530, 1230), f': {id}', fill=(0, 0, 139))
-    draw.text((1530, 1230), f': {id}', fill=None, font=font, stroke_fill=(255, 255, 255), stroke_width=0)
+    draw.text((1600, 1300), f': {id}', fill=(0, 0, 139))
+    draw.text((1600, 1300), f': {id}', fill=None, font=font, stroke_fill=(255, 255, 255), stroke_width=0)
     
     # Draw user's username with white fill and green border
-    draw.text((2000, 1500), f': {uname}', fill=(255, 255, 255), font=font)
-    draw.text((2000, 1500), f': {uname}', fill=None, font=font, stroke_fill=(0, 128, 0), stroke_width=6)
+    draw.text((2000, 1520), f': {uname}', fill=(255, 255, 255), font=font)
+    draw.text((2000, 1520), f': {uname}', fill=None, font=font, stroke_fill=(0, 128, 0), stroke_width=6)
     
     # Resize photo and position
     pfp_position = (255, 323)
