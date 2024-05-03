b4fc1b0ebb9b615f8034785313124faef670161e
Akash <126413936+crazyworld-izzy@users.noreply.github.com>
2024-05-03 22:07:25 +0530

    Update pm.py

--- VIPMUSIC/plugins/tools/pm.py
+++ VIPMUSIC/plugins/tools/pm.py
@@ -13,12 +13,7 @@ def goodnight_command_handler(_, message):
         app.send_sticker(message.chat.id, sticker_id)
         message.reply_text(f"**odi poda baddu paiya, {sender}enna eanda pm kupdara 🤬🤬**")
     else:
-        emoji = get_random_emoji()
-        app.send_message(message.chat.id, emoji)
-        message.reply_text(f"**odi poda baddu paiya, {sender}nna eanda pm kupdara 🤬🤬**")
-    else: {emoji}**")
-
-
+    
 def get_random_sticker():
     stickers = [
         "CAACAgIAAx0CerTvRQACFMZmNQ-LfqqGQRXrcIVN3R4rOjMi3QAC_R8AAp5mwUuLKHqA38PyoB4E", # Sticker 1
