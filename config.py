629da0f5d96a7d0b118b3e6122f67288fd4388c7
Akash <126413936+crazyworld-izzy@users.noreply.github.com>
2024-05-07 17:04:45 +0530

    Update config.py

--- config.py
+++ config.py
@@ -10,7 +10,7 @@ load_dotenv()
 API_ID = int(getenv("API_ID", "10284859"))
 API_HASH = getenv("API_HASH", "b0ad58eb8b845ba0003e0d9ce5fc2196")
 # Get your token from @BotFather on Telegram.
-BOT_TOKEN = getenv("BOT_TOKEN", "6009759960:AAEKPNd6sssszLHc7SGNWIEZpwk7kiw9gIo")
+BOT_TOKEN = getenv("BOT_TOKEN", "6009759960:AAGPgxmfzfpTfUDGT3POlpraTnZiqtxr0GE")
 
 # Get your mongo url from cloud.mongodb.com
 MONGO_DB_URI = getenv("MONGO_DB_URI", "mongodb+srv://monivps5:monivps5@cluster0.kmbq8we.mongodb.net/?retryWrites=true&w=majority")
