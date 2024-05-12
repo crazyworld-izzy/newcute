147b07eceadfc0e6f1d023c2941e256a74ced4bf
Akash <126413936+crazyworld-izzy@users.noreply.github.com>
2024-05-12 14:19:15 +0530

    Update logo.py

--- VIPMUSIC/plugins/misc/logo.py
+++ VIPMUSIC/plugins/misc/logo.py
@@ -5,14 +5,14 @@ import pyrogram, asyncio, random, time
 from pyrogram.errors import FloodWait
 from pyrogram.types import *
 import requests
-from ANNIEMUSIC import app
+from VIPMUSIC import app
 
 
 
 @app.on_message(filters.command("logo"))
 async def logo(app, msg: Message):
     if len(msg.command) == 1:
-       return await msg.reply_text("Usage:\n\n /logo jarvis")
+       return await msg.reply_text("Usage:\n\n /logo Cutegirl")
     logo_name = msg.text.split(" ", 1)[1]
     API = f"https://api.sdbots.tech/logohq?text={logo_name}"
     req = requests.get(API).url
@@ -22,7 +22,7 @@ async def logo(app, msg: Message):
 @app.on_message(filters.command("animelogo"))
 async def logo(app, msg: Message):
     if len(msg.command) == 1:
-       return await msg.reply_text("Usage:\n\n /animelogo jarvis")
+       return await msg.reply_text("Usage:\n\n /animelogo Cutegirl")
     logo_name = msg.text.split(" ", 1)[1]
     API = f"https://api.sdbots.tech/anime-logo?name={logo_name}"
     req = requests.get(API).url
